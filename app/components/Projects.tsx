43c6689f34a9aac6c1c3b086c2ae59506ad81881
Your Name <you@example.com>
2026-01-16 13:25:13 +0530

    update something

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -18,7 +18,7 @@ const featuredProjects: Project[] = [
 
 export default function Projects(): React.JSX.Element {
   return (
-    <section id="lab" className="py-20 px-6">
+    <section id="lab" className="py-18 px-6">
       <div className="container mx-auto max-w-7xl">
         {featuredProjects.map((project, index) => {
           const isEven = index % 2 === 1;
